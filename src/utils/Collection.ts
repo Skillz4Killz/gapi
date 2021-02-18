cc762f0b94e907d6b521a3f17bb894ddfd4ff65b
Skillz <gankstarsleague@gmail.com>
2021-02-17 20:56:19 -0500

    feat: add collection class

---
+++ src/utils/Collection.ts
@@ -0,0 +1,93 @@
+import Client from '../Client';
+import { chooseRandom } from './random';
+
+export default class Collection<K, V> extends Map<K, V> {
+  maxSize?: number;
+  client: Client;
+
+  constructor(client: Client) {
+    super();
+    this.client = client;
+  }
+
+  get array() {
+    return [...this.values()];
+  }
+
+  get first(): V {
+    return this.values().next().value;
+  }
+
+  get last(): V {
+    return this.array[this.size - 1]!;
+  }
+
+  get random() {
+    return chooseRandom([...this.values()]);
+  }
+
+  set(key: K, value: V) {
+    // When this collection is maxSizeed make sure we can add first
+    if ((this.maxSize || this.maxSize === 0) && this.size >= this.maxSize) {
+      return this;
+    }
+
+    return super.set(key, value);
+  }
+
+  find(callback: (value: V, key: K) => boolean) {
+    for (const key of this.keys()) {
+      const value = this.get(key)!;
+      if (callback(value, key)) return value;
+    }
+    // If nothing matched
+    return;
+  }
+
+  filter(callback: (value: V, key: K) => boolean) {
+    const relevant = new Collection<K, V>();
+    this.forEach((value, key) => {
+      if (callback(value, key)) relevant.set(key, value);
+    });
+
+    return relevant;
+  }
+
+  map<T>(callback: (value: V, key: K) => T) {
+    const results = [];
+    for (const key of this.keys()) {
+      const value = this.get(key)!;
+      results.push(callback(value, key));
+    }
+    return results;
+  }
+
+  some(callback: (value: V, key: K) => boolean) {
+    for (const key of this.keys()) {
+      const value = this.get(key)!;
+      if (callback(value, key)) return true;
+    }
+
+    return false;
+  }
+
+  every(callback: (value: V, key: K) => boolean) {
+    for (const key of this.keys()) {
+      const value = this.get(key)!;
+      if (!callback(value, key)) return false;
+    }
+
+    return true;
+  }
+
+  reduce<T>(callback: (accumulator: T, value: V, key: K) => T, initialValue?: T): T {
+    let accumulator: T = initialValue!;
+
+    for (const key of this.keys()) {
+      const value = this.get(key)!;
+      accumulator = callback(accumulator, value, key);
+    }
+
+    return accumulator;
+  }
+}
