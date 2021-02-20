b2c33b6200f4c658b864565951ecd4f285daea03
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-02-20 20:03:37 +0000

    feat: initialize tasks with colors

--- src/framework/Client.ts
+++ src/framework/Client.ts
@@ -1,6 +1,7 @@
 import { baseStructures } from '../base';
 import Client, { ClientOptions } from '../Client';
 import Message from '../lib/Message';
+import { bgBlue, bgYellow, black } from '../utils/colorette';
 import { Command } from './Command';
 import Event from './Event';
 import Monitor from './Monitor';
@@ -32,6 +33,7 @@ export default class BotClient extends Client {
     // TODO: Load all end user files
 
     this.initializeMessageListener();
+    this.initializeTasks();
   }
 
   /** Handler that is run on messages and can  */
@@ -52,7 +54,43 @@ export default class BotClient extends Client {
 
   /** Allows users to override and customize the addition of a event listener */
   initializeMessageListener() {
-      this.on('messageCreated', (message) => this.processMonitors(message))
+    this.on('messageCreated', message => this.processMonitors(message));
+  }
+
+  /** Allows usersto override and customize the initialization of tasks */
+  initializeTasks() {
+    this.tasks.forEach(async task => {
+      // THESE TASKS MUST RUN WHEN STARTING BOT
+      if (task.executeOnStartup) await task.execute().catch(console.log);
+
+      // WILL CREATE THE INTERVAL AT THE TIME THIS TASK SHOULD RUN.
+      setTimeout(async () => {
+        console.log(`${bgBlue(`[${this.getTime()}]`)} => [TASK: ${bgYellow(black(task.name))}] Started.`);
+        await task.execute().catch(console.log);
+
+        setInterval(async () => {
+          console.log(`${bgBlue(`[${this.getTime()}]`)} => [TASK: ${bgYellow(black(task.name))}] Started.`);
+          await task.execute().catch(console.log);
+        }, task.intervalMilliseconds);
+        // THE AMOUNT OF TIME UNTIL THE NEXT EXECUTION POINT
+      }, Date.now() % task.intervalMilliseconds);
+    });
+  }
+
+  /** Gets the current time, in a human readable format. Useful for logs. */
+  getTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minute = now.getMinutes();
+
+    let hour = hours;
+    let amOrPm = `AM`;
+    if (hour > 12) {
+      amOrPm = `PM`;
+      hour = hour - 12;
+    }
+
+    return `${hour >= 10 ? hour : `0${hour}`}:${minute >= 10 ? minute : `0${minute}`} ${amOrPm}`;
   }
 }
 
