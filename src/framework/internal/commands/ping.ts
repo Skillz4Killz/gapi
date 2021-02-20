b661b178edeab8ed614803da3101b5d7a8284903
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-02-20 20:02:29 +0000

    feat: internal ping command

---
+++ src/framework/internal/commands/ping.ts
@@ -0,0 +1,12 @@
+import Message from '../../../lib/Message';
+import { Command } from '../../Command';
+
+export default class extends Command {
+  name = 'ping';
+  aliases = ['pong'];
+  description = 'Ping! Check if the bot is responsive!';
+
+  async execute(message: Message) {
+    message.send(`Ping MS: ${Date.now() - message.timestamp}ms`);
+  }
+}
