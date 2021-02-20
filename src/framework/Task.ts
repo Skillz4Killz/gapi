bac964cd1710fd68ec846e9a111093656386dc11
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-02-20 14:18:29 +0000

    feat: add tasks class

---
+++ src/framework/Task.ts
@@ -0,0 +1,26 @@
+import BotClient from "./Client";
+
+export default class Task {
+    /** The client itself */
+    client: BotClient;
+    /** The name of the task. */
+    name: string;
+    /** The interval to run this task in milliseconds. By default, this is 1 minute. */
+    intervalMilliseconds = 60000;
+
+    constructor(client: BotClient, name: string) {
+        this.client = client;
+        this.name = name;
+
+        this.client.tasks.set(name, this);
+    }
+
+    async execute() {
+        // Placeholder for the execution handler for this task.
+    }
+
+    async init() {
+        // Placeholder for anything you would like to run when this task is first created.
+        // For example, making sure some tables exist in the database.
+    }
+}
