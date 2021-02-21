d8b539f96be4c32bc204db77376766788eea324c
Skillz4Killz <admin@ezlgg.com>
2021-02-20 20:04:37 -0500

    feat: add argument class

---
+++ src/framework/Argument.ts
@@ -0,0 +1,24 @@
+import Message from '../lib/Message';
+import BotClient from './Client';
+import { Command, CommandArgument } from './Command';
+
+export default class Argument {
+  /** The client itself */
+  client: BotClient;
+  /** The name of the argument. */
+  name: string;
+
+  constructor(client: BotClient, name: string) {
+    this.client = client;
+    this.name = name;
+  }
+
+  async execute(parameters: string[], message: Message, command: Command, arg: CommandArgument): Promise<any> {
+    // Placeholder for the function that will run.
+    if (parameters || message || command || arg) return;
+  }
+
+  async init() {
+    // Placeholder for when an argument is initialized.
+  }
+}
