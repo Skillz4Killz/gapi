2ebd8d3c82ca6c691c7a499d1b3eaa87fdf04640
Skillz4Killz <admin@ezlgg.com>
2021-02-20 21:33:20 -0500

    feat:add walk function

--- src/utils/walk.ts
+++ src/utils/walk.ts
@@ -1,11 +1,16 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
 /** Walks through a directory allowing you to process it with a for await loop */
 export async function* walk(dir: string): AsyncGenerator<any, any, unknown> {
-    for await (const d of await fs.promises.opendir(dir)) {
-        const entry = path.join(dir, d.name);
-        if (d.isDirectory()) yield* await walk(entry);
-        else if (d.isFile()) yield [d.name, require(entry)];
-    }
+  const folder = await fs.promises.opendir(dir).catch(console.log);
+  if (!folder) return;
+
+  for await (const d of folder) {
+    const entry = path.join(dir, d.name);
+    if (!entry.endsWith('.js')) continue;
+    
+    if (d.isDirectory()) yield* await walk(entry);
+    else if (d.isFile()) yield [d.name, require(entry)];
+  }
 }
