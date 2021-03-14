3eba92456fe4c2295d41863154584e596b5b7363
Skillz <gankstarsleague@gmail.com>
2021-03-14 12:14:21 -0400

    feat: i18next and reaction collectors

--- src/framework/Client.ts
+++ src/framework/Client.ts
@@ -1,3 +1,4 @@
+import i18next from 'i18next';
 import path from 'path';
 import { baseStructures } from '../base';
 import Client, { ClientOptions } from '../Client';
@@ -28,11 +29,16 @@ export default class BotClient extends Client {
   prefix: string;
   /** The bot's prefixes per team. <teamId, prefix> */
   prefixes = new Map<string, string>();
+  /** The languages per team. <teamId, language> */
+  languages = new Map<string, string>();
+  /** The path that the end users commands,monitors, inhibitors and others will be located. */
+  sourceFolderPath: string;
 
   constructor(options: BotClientOptions) {
     super(options);
 
     this.prefix = options.prefix;
+    this.sourceFolderPath = options.sourceFolderPath || path.join(process.cwd(), 'src/');
 
     this.init();
   }
@@ -59,10 +65,34 @@ export default class BotClient extends Client {
       }),
     );
 
-    // TODO: Load all end user files
+    // Load all end user files
+    await Promise.all(
+      [
+        ['arguments', this.arguments] as const,
+        ['commands', this.commands] as const,
+        ['inhibitors', this.inhibitors] as const,
+        ['monitors', this.monitors] as const,
+        ['tasks', this.tasks] as const,
+      ].map(async ([dir, collection]) => {
+        console.log(path.join(this.sourceFolderPath, dir));
+        for await (const result of walk(path.join(this.sourceFolderPath, dir))) {
+          if (!result) return;
+
+          const [filename, file] = result;
+          const name = filename.substring(0, filename.length - 2);
+          const piece = file.default ? new file.default(this, name) : new file(this, name);
+          collection.set(piece.name || name, piece);
+          if (piece.init) await piece.init();
+        }
+      }),
+    );
 
     this.initializeMessageListener();
     this.initializeTasks();
+    this.on('reactionAdded', payload => {
+      // TODO: convert payload into message
+      this.processReactionCollector(payload);
+    });
   }
 
   /** Handler that is run on messages and can  */
@@ -96,11 +126,11 @@ export default class BotClient extends Client {
 
       // WILL CREATE THE INTERVAL AT THE TIME THIS TASK SHOULD RUN.
       setTimeout(async () => {
-        console.log(`${bgBlue(`[${this.getTime()}]`)} => [TASK: ${bgYellow(black(task.name))}] Started.`);
+        console.log(`${bgBlue(`[${this.getTime()}]`)} [TASK: ${bgYellow(black(task.name))}] Started.`);
         await task.execute().catch(console.log);
 
         setInterval(async () => {
-          console.log(`${bgBlue(`[${this.getTime()}]`)} => [TASK: ${bgYellow(black(task.name))}] Started.`);
+          console.log(`${bgBlue(`[${this.getTime()}]`)} [TASK: ${bgYellow(black(task.name))}] Started.`);
           await task.execute().catch(console.log);
         }, task.intervalMilliseconds);
         // THE AMOUNT OF TIME UNTIL THE NEXT EXECUTION POINT
@@ -129,8 +159,29 @@ export default class BotClient extends Client {
     // PLACEHOLDER JUST TO SHUT UP THE ERRORS. THIS SHOULD BE CUSTOMIZED BY USER
     if (!message) console.log('command failed', command, error);
   }
+
+  /** This function helps translate the string to the specific teams needs. */
+  translate(teamId: string, key: string, options?: Record<string, unknown>, returnArray?: true): string[];
+  translate(teamId: string, key: string, options?: Record<string, unknown>, returnArray?: false): string;
+  translate(teamId: string, key: string, options?: Record<string, unknown>, returnArray = false) {
+    const team = this.teams.get(teamId);
+    const language = this.languages.get(teamId) || team?.locale || 'en_US';
+
+    // undefined is silly bug cause i18next dont have proper typings
+    const languageMap =
+      i18next.getFixedT(language.replace('-', '_'), undefined) || i18next.getFixedT('en_US', undefined);
+
+    if (returnArray) return languageMap(key, { ...options, returnObjects: true }) as string[];
+    return languageMap(key, options) as string;
+  }
+
+  processReactionCollector(message: Message) {
+    // TODO: forward to channel.processReactionCollector
+    console.log(message);
+  }
 }
 
 export interface BotClientOptions extends ClientOptions {
   prefix: string;
+  sourceFolderPath?: string;
 }
