e8b73cc8a7cd27393243a78487195e15b4fd69cb
Skillz <gankstarsleague@gmail.com>
2021-03-14 17:52:02 -0400

    feat: fix walk and add directoryNames

--- src/utils/walk.ts
+++ src/utils/walk.ts
@@ -8,9 +8,24 @@ export async function* walk(dir: string): AsyncGenerator<any, any, unknown> {
 
   for await (const d of folder) {
     const entry = path.join(dir, d.name);
-    if (!entry.endsWith('.js')) continue;
-    
     if (d.isDirectory()) yield* await walk(entry);
+    
+    if (!entry.endsWith('.js')) continue;
+
     else if (d.isFile()) yield [d.name, require(entry)];
   }
+}
+
+/** Walks through a directory selecting the directory names */
+export async function directoryNames(dir: string) {
+  const folder = await fs.promises.opendir(dir).catch(console.log);
+  if (!folder) return [];
+
+  const directories: string[] = [];
+
+  for await (const d of folder) {
+    if (d.isDirectory()) directories.push(d.name);
+  }
+
+  return directories;
 }
