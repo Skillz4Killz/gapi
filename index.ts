de541b938d27d00aca2923c3863b2acea8e04978
Skillz <gankstarsleague@gmail.com>
2021-03-15 10:25:50 -0400

    feat: export in index.ts

--- index.ts
+++ index.ts
@@ -1,18 +1,35 @@
-export * from "./src/Client";
+// FRAMEWORK
+export * from './src/framework/Argument';
+export * from './src/framework/Client';
+export * from './src/framework/Command';
+export * from './src/framework/Event';
+export * from './src/framework/Inhibitor';
+export * from './src/framework/Monitor';
+export * from './src/framework/Task';
 
-export * from "./src/lib/Base";
-export * from "./src/lib/Message";
-export * from "./src/lib/Team";
-export * from "./src/lib/User";
+// LIBRARY
 
-export * from "./src/rest/RequestManager";
-export * from "./src/rest/endpoints";
+export * from './src/Client';
 
-export * from "./src/utils/Collection";
-export * from "./src/utils/delay";
-export * from "./src/utils/random";
+export * from './src/lib/Base';
+export * from './src/lib/Channel';
+export * from './src/lib/ChannelMessages';
+export * from './src/lib/Message';
+export * from './src/lib/Permissions';
+export * from './src/lib/Team';
+export * from './src/lib/User';
 
-export * from "./src/websocket/Events";
-export * from "./src/websocket/Shard";
-export * from "./src/websocket/WebsocketManager";
+export * from './src/rest/RequestManager';
+export * from './src/rest/endpoints';
 
+export * from './src/utils/Collection';
+export * from './src/utils/colorette';
+export * from './src/utils/delay';
+export * from './src/utils/Embed';
+export * from './src/utils/random';
+export * from './src/utils/time';
+export * from './src/utils/walk';
+
+export * from './src/websocket/Events';
+export * from './src/websocket/Shard';
+export * from './src/websocket/WebsocketManager';
